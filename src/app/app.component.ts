c2a18e1d768921968cf07dd33818f64d6956a543
ValverdeAcosta <vixen010101@gmail.com>
2022-05-02 11:10:03 +0200

    Remove app.component.css and change img size

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: []
 })
 export class AppComponent {
   logo: string = "/assets/images/NASA-Logo.png";
