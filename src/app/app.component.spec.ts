72a50c7d1e3bf579c65e74e05a7a9c7896c215cb
ValverdeAcosta <vixen010101@gmail.com>
2022-05-02 11:31:09 +0200

    fix app-test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -21,11 +21,4 @@ describe('AppComponent', () => {
     const app = fixture.componentInstance;
     expect(app.logo).toContain('NASA');
   });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('nasa-news app is running!');
-  });
 });
