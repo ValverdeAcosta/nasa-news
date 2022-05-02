ba80aa37af9c09ea5046b443d213136c6c2a3707
ValverdeAcosta <vixen010101@gmail.com>
2022-05-02 11:03:36 +0200

    Detail's page created

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router'; // CLI imports router
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { DetailsComponent } from './pages/details/details.component';
+
 
 const routes: Routes = [
     { path: 'dashboard', component: DashboardComponent },
+    { path: 'details', component: DetailsComponent },
 ];
 
 @NgModule({
