5caae238df53bb384df20c9e28f563fd5bd8a4d6
ValverdeAcosta <vixen010101@gmail.com>
2022-05-02 10:39:49 +0200

    Main page and routing set up

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -19,7 +19,7 @@ describe('AppComponent', () => {
   it(`should have as title 'nasa-news'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('nasa-news');
+    expect(app.logo).toContain('NASA');
   });
 
   it('should render title', () => {
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'nasa-news';
+  logo: string = "/assets/images/NASA-Logo.png";
 }
